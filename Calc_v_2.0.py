3841a35f0e024c1e66c72c9cc2da5b0e3c575013
BahRuslan <bakhmetevruslan@gmail.com>
2022-07-19 20:01:32 +0300

    improved calculator

--- Calc_v_2.0.py
+++ Calc_v_2.0.py
@@ -1,35 +1,53 @@
+# Модифицировал калькулятор, добавил: основной код обернул в функцию, добавил повторение работы
 
 from colorama import init
 from colorama import Fore, Back, Style
 init()
 
-print(Fore.BLACK)
-print(Back.YELLOW)
-what = input("Что делаем? (+,-,*,/):")
 
-print(Back.GREEN)
-a = float(input("Введите первое число: "))
-b = float(input("Введите второе число: "))
+def computing():
+    print(Fore.BLACK)
+    print(Back.YELLOW)
+    what = input("Что делаем? (+,-,*,/):")
+    while True:
+        if what not in "+-/*":
+            print(Back.RED)
+            print("Введено не верное значение!\nВведите значения + или - или * или /")
+            what = input("Что делаем? (+,-,*,/):")
+            continue
+        else:
+            break
 
-print(Back.CYAN)
-if what == "+":
-    c = a + b
-    print("Результат: " + str(c))
-elif what == "-":
-    c = a - b
-    print("Результат: " + str(c))
-if what == "*":
-    c = a * b
-    print("Результат: " + str(c))
-elif what == "/":
-    c = a / b
-    print("Результат: " + str(c))
+    print(Back.GREEN)
+    a = float(input("Введите первое число: "))
+    b = float(input("Введите второе число: "))
+    print(Back.CYAN)
+    if what == "+":
+        print(f"Результат: {a + b}")
+    if what == "-":
+        print(f"Результат: {a - b}")
+    if what == "*":
+        print(f"Результат: {a * b}")
+    if what == "/":
+        print(f"Результат: {a / b}")
 
-print(Back.MAGENTA)
-input("Это моя первая работа.Ну как тебе калькулятор?")
 
-print(Back.RED)
-print("Большое спасибо за Ваш комментарий)))")
+computing()
+
+print(Back.BLUE)
+again = input("Хотите продолжить вычисления?(д - да, н - нет): ")
+while True:
+    if again == "д":
+        computing()
+    if again == "н":
+        print("Если нужно будет что-то посчитать, возвращайтесь")
+        break
+    else:
+        print("Вводите только 'д' что означает да или 'н' что означает нет ")
+        again = input("Хотите продолжить вычисления?(д - да, н - нет): ")
+        continue
+
+
 
 
 
